29d9abb061824e415b865af3d1f83fe9559d1df6
SkyriMeg <m-pilon@orange.fr  git config --global user.name SkyriMeggit config --global user.name Meggit config --global user.email m-pilon@orange.fr  git config --global user.email m-pilon@orange.fr>
2022-11-29 16:46:46 +0100

    Atelier terminé

--- src/DataFixtures/SquirelFixture.php
+++ src/DataFixtures/SquirelFixture.php
@@ -2,19 +2,32 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Nut;
 use App\Entity\Squirel;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class SquirelFixture extends Fixture
 {
-    public function load(ObjectManager $manager)
-{
-	for ($i = 0; $i < 5; $i++) {
-		$squirel = new Squirel();
-		$squirel->setName('name' . $i);
-		$manager->persist($squirel);
+	public const NUTS = [
+		['name' => 'Noix', 'stock' => 129],
+		['name' => 'Noisette', 'stock' => 364],
+		['name' => 'Amande', 'stock' => 53],
+	];
+
+	public function load(ObjectManager $manager)
+	{
+		for ($i = 0; $i < 5; $i++) {
+			$squirel = new Squirel();
+			$squirel->setName('name' . $i);
+			$manager->persist($squirel);
+		}
+		foreach (self::NUTS as $nut) {
+			$newNut = new Nut();
+			$newNut->setName($nut['name']);
+			$newNut->setStock($nut['stock']);
+			$manager->persist($newNut);
+		}
+		$manager->flush();
 	}
-	$manager->flush();
-}
 }
